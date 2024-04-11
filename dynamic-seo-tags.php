0a4c0c16bdb2efe7197e40b4362dfb9cd94e22af
Hans Steffens <hans@steffens.co>
2024-04-10 22:39:45 -0500

    Add Support for Twitter and OpenGraph meta tags

--- dynamic-seo-tags.php
+++ dynamic-seo-tags.php
@@ -42,8 +42,8 @@ function dynamic_seo_tags_page()
                 <tr valign="top">
                     <th scope="row">SEO Meta Description</th>
                     <td>
-						<textarea name="seo_custom_text" rows="5" cols="50"><?php echo esc_textarea(get_option('seo_custom_text')); ?></textarea>
-					</td>
+                        <textarea name="seo_custom_text" rows="5" cols="50"><?php echo esc_textarea(get_option('seo_custom_text')); ?></textarea>
+                    </td>
                 </tr>
 
                 <tr valign="top">
@@ -60,34 +60,73 @@ function dynamic_seo_tags_page()
 
 // Filter for Yoast SEO meta description
 add_filter('wpseo_metadesc', 'dynamic_seo_meta_description', 10, 1);
-function dynamic_seo_meta_description($description) {
+function dynamic_seo_meta_description($description)
+{
     return dynamic_seo_tags_replace('description', $description);
 }
 
 // Filter for Yoast SEO title
 add_filter('wpseo_title', 'dynamic_seo_title', 10, 1);
-function dynamic_seo_title($title) {
+function dynamic_seo_title($title)
+{
     return dynamic_seo_tags_replace('title', $title);
 }
 
 // Filter for Yoast SEO canonical
 add_filter('wpseo_canonical', 'dynamic_seo_canonical', 10, 1);
-function dynamic_seo_canonical($canonical) {
+function dynamic_seo_canonical($canonical)
+{
     return dynamic_seo_tags_replace('canonical', $canonical, true);
 }
 
+// Filter for Yoast SEO OpenGraph title
+add_filter('wpseo_opengraph_title', 'dynamic_seo_og_title', 10, 1);
+function dynamic_seo_og_title($title)
+{
+    return dynamic_seo_tags_replace('title', $title);
+}
+
+// Filter for Yoast SEO OpenGraph description
+add_filter('wpseo_opengraph_desc', 'dynamic_seo_og_description', 10, 1);
+function dynamic_seo_og_description($description)
+{
+    return dynamic_seo_tags_replace('description', $description);
+}
+
+// Filter for Yoast SEO OpenGraph URL
+add_filter('wpseo_opengraph_url', 'dynamic_seo_og_url', 10, 1);
+function dynamic_seo_og_url($url)
+{
+    return dynamic_seo_tags_replace('url', $url, true);
+}
+
+// Filter for Yoast SEO Twitter title
+add_filter('wpseo_twitter_title', 'dynamic_seo_twitter_title', 10, 1);
+function dynamic_seo_twitter_title($title)
+{
+    return dynamic_seo_tags_replace('title', $title);
+}
+
+// Filter for Yoast SEO Twitter description
+add_filter('wpseo_twitter_description', 'dynamic_seo_twitter_description', 10, 1);
+function dynamic_seo_twitter_description($description)
+{
+    return dynamic_seo_tags_replace('description', $description);
+}
+
 // Common function to replace tags
-function dynamic_seo_tags_replace($type, $content, $is_url = false) {
+function dynamic_seo_tags_replace($type, $content, $is_url = false)
+{
     $variable_name = get_option('seo_variable_name');
     if (isset($_GET[$variable_name])) {
-		$variable_value_raw = $_GET[$variable_name];
-		// Replace dashes with spaces and capitalize each word
+        $variable_value_raw = $_GET[$variable_name];
+        // Replace dashes with spaces and capitalize each word
         $variable_value = htmlspecialchars(ucwords(str_replace('-', ' ', $variable_value_raw)), ENT_QUOTES, 'UTF-8');
-        
+
         // Decide which custom text to use based on the type
         $custom_text = ($type == 'title') ? get_option('seo_custom_title') : get_option('seo_custom_text');
-        
-        if(!$is_url) {
+
+        if (!$is_url) {
             // For description and title
             $content = str_replace("[$variable_name]", $variable_value, $custom_text);
         } else {
@@ -98,7 +137,8 @@ function dynamic_seo_tags_replace($type, $content, $is_url = false) {
     return $content;
 }
 
-function handle_url_param_shortcode($atts) {
+function handle_url_param_shortcode($atts)
+{
     // Define default attributes and merge with user attributes
     $attributes = shortcode_atts(array(
         'variable' => '', // Variable name to look for in the URL
@@ -112,7 +152,7 @@ function handle_url_param_shortcode($atts) {
     } else {
         $variable_value_raw = sanitize_text_field($attributes['default']); // Use user-defined default
     }
-    
+
     // Format the output: replace dashes with spaces and capitalize each word
     return htmlspecialchars(ucwords(str_replace('-', ' ', $variable_value_raw)), ENT_QUOTES, 'UTF-8');
 }
